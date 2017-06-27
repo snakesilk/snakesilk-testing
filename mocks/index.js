4b0e6674b57d7b33411ef94f8091ab1154ba86d6
Pontus Persson <pontus.alexander@gmail.com>
2017-06-28 00:14:00 +0200

    Initial commit.

---
+++ mocks/index.js
@@ -0,0 +1,18 @@
+const AudioContext = require('./AudioContext');
+const Canvas = require('./Canvas');
+const DOMNode = require('./DOMNode');
+const requestAnimationFrame = require('./requestAnimationFrame');
+
+const Math = require('./math');
+const THREE = require('./three');
+
+module.exports = {
+  AudioContext,
+  Canvas,
+  DOMNode,
+  Math,
+  requestAnimationFrame,
+  THREE,
+};
+
+console.log(module.exports);
